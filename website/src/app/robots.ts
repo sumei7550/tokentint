24264f08c44ec17515ca1e31b2a4bbdc243a31c2
sumei7550 <285894878+sumei7550@users.noreply.github.com>
2026-08-15 22:38:49 +0800

    修复站点地图无法抓取问题

--- website/src/app/robots.ts
+++ website/src/app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: '*',
-        allow: '/',
+        allow: ['/upgrade?*_rsc=*', '/'],
         disallow: ['/api/', '/upgrade', '/success', '/restore'],
       },
     ],
